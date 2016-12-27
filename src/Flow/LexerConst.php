4502f19c685dafd6a018f598bc5dba9964eb1b09
rez1dent3 <maksim.babichev95@gmail.com>
2016-12-27 15:08:34 +0300

    refactor lexerConst

--- src/Flow/LexerConst.php
+++ src/Flow/LexerConst.php
@@ -4,57 +4,31 @@ namespace Deimos\Flow;
 
 class LexerConst
 {
-
-    // foreach
-    const T_FOREACH      = 'foreach';
+    const T_FOREACH      = 'foreach';       // foreach
     const T_FOREACH_ELSE = 'foreachelse';
     const T_END_FOREACH  = '/foreach';
-
-    // for
-    const T_FOR     = 'for';
-    const T_END_FOR = '/for';
-
-    // while
-    const T_WHILE     = 'while';
-    const T_END_WHILE = '/while';
-
-    const T_CONTINUE = 'continue';
-    const T_BREAK    = 'break';
-
-    // if|else|elseif
-    const T_IF     = 'if';
-    const T_ELSE   = 'else';
-    const T_ELSEIF = 'elseif';
-    const T_END_IF = '/if';
-
-    // try|catch|finally
-    const T_TRY     = 'try';
-    const T_CATCH   = 'catch';
-    const T_FINALLY = 'finally';
-    const T_END_TRY = '/try';
-
-    // php
-    const T_PHP     = 'php';
-    const T_END_PHP = '/php';
-
-    // assign
-    const T_ASSIGN = 'assign';
-
-    // variable
-    const T_VARIABLE = 'variable';
-
-    // include
-    const T_INCLUDE = 'include';
-
-    // partial
-    const T_PARTIAL = 'partial';
-
-    // extends
-    const T_EXTENDS = 'extends';
-
-    // partial
-    const T_BLOCK_START = 'block';
-    const T_BLOCK_END   = '/block';
-    const T_BLOCK       = 'printBlock';
-
+    const T_FOR          = 'for';           // for
+    const T_END_FOR      = '/for';
+    const T_WHILE        = 'while';         // while
+    const T_END_WHILE    = '/while';
+    const T_CONTINUE     = 'continue';      // operators
+    const T_BREAK        = 'break';
+    const T_IF           = 'if';            // if|else|elseif
+    const T_ELSE         = 'else';
+    const T_ELSEIF       = 'elseif';
+    const T_END_IF       = '/if';
+    const T_TRY          = 'try';           // try|catch|finally
+    const T_CATCH        = 'catch';
+    const T_FINALLY      = 'finally';
+    const T_END_TRY      = '/try';
+    const T_PHP          = 'php';           // php
+    const T_END_PHP      = '/php';
+    const T_ASSIGN       = 'assign';        // assign
+    const T_VARIABLE     = 'variable';      // variable
+    const T_INCLUDE      = 'include';       // include
+    const T_PARTIAL      = 'partial';       // partial
+    const T_EXTENDS      = 'extends';       // extends
+    const T_BLOCK_START  = 'block';         // block
+    const T_BLOCK_END    = '/block';
+    const T_BLOCK        = 'printBlock';
 }
